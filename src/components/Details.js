09ac85541f57d98e544a0bb93196d6b4fc3b2e5f
alexanderjacks <alexanderthejacks@gmail.com>
2019-03-09 03:01:25 -0800

    :wrench: rebuilding for client app

--- src/components/Details.js
+++ src/components/Details.js
@@ -8,8 +8,8 @@ const Home = () => {
   return (
     <div>
       <h1>Profile Details</h1>
+      <p>Name: {user.name}</p>
       <p>Email: {user.email}</p>
-      <p>Phone: {user.phone_number}</p>
       <p>Username: {user.username}</p>
       <Link to="/app/home">Home</Link>
     </div>
